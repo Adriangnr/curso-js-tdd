18c7d8a4dc9e7138914624089b434b557265c9e3
amansilla <adrian.mansilla@avantrip.com>
2018-08-30 18:09:16 -0300

    agrego fizzbuzz

---
+++ js-tdd-course/tests/fizzBuzz.spec.js
@@ -0,0 +1,14 @@
+/*
+Si el numero es divisible por 3, escribe Fizz
+Si el numero es divisible por 5, escribe Buzz
+Si el numero es divisible por 3 y por 5 escribe FizzBuzz
+Si el numero no es multiplo de ninguno, retorna el numero
+*/
+
+import { expect } from 'chai'
+
+describe('FizzBuzz', () => {
+  it('Should return Fizz when multiple of 3', () => {
+    
+  })
+})
