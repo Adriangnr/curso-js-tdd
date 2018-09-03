db8a5c71e1a144e7131fd73b36e12999c8e5c539
amansilla <adrian.mansilla@avantrip.com>
2018-09-03 17:10:31 -0300

    fizzbuzz

--- js-tdd-course/tests/fizzBuzz.spec.js
+++ js-tdd-course/tests/fizzBuzz.spec.js
@@ -7,8 +7,28 @@ Si el numero no es multiplo de ninguno, retorna el numero
 
 import { expect } from 'chai'
 
+const fizzbuzz = require('../src/fizzBuzz')
+
 describe('FizzBuzz', () => {
   it('Should return Fizz when multiple of 3', () => {
-    
+    expect(fizzbuzz(3)).to.be.equal('Fizz')
+    expect(fizzbuzz(6)).to.be.equal('Fizz')
+    expect(fizzbuzz(12)).to.be.equal('Fizz')
+  })
+
+  it('Should return Buzz when multiple of 5', () => {
+    expect(fizzbuzz(5)).to.be.equal('Buzz')
+    expect(fizzbuzz(10)).to.be.equal('Buzz')
+    expect(fizzbuzz(25)).to.be.equal('Buzz')
+  })
+
+  it('Should return FizzBuzz when multiple of 3 and 5', () => {
+    expect(fizzbuzz(15)).to.be.equal('FizzBuzz')
+    expect(fizzbuzz(30)).to.be.equal('FizzBuzz')
+    expect(fizzbuzz(75)).to.be.equal('FizzBuzz')
+  })
+
+  it('Should return empty string when not multiple of 3 or 5', () => {
+    expect(fizzbuzz(7)).to.be.equal('')
   })
 })
