7ee351a59a1e90a21ec6199f4a392e6f02c159ad
amansilla <adrian.mansilla@avantrip.com>
2018-10-17 16:28:29 -0300

    agrego cliente.

---
+++ wrapper-spotify-client/src/Spotify.js
@@ -0,0 +1,7 @@
+import SpotifyWrapper from 'spotify-wrapper';
+
+const spotify = new SpotifyWrapper({
+  token: 'BQAfzvTjWcjzYHVRS1hnpdGcH4APv88gghwHVxQ68yo45yD8GN94SJsjibp8LDxovI7QUJkFMFXfr6V_0EBVPs7qvQLUyU8EJ4SRH9sMatDJ-PdyizDrGxqC-3kJgw3AOXZ9DBzQT0dGrGa2o4E',
+});
+
+export default spotify;
